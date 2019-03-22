f9e9c044113c7beeb760dd12f40c7dcd02df9e1b
johnzaro <johnzrgnns@gmail.com>
2019-03-22 13:25:17 +0200

    set up gradle

---
+++ src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module main {
+
+	requires javafx.controls;
+	requires javafx.graphics;
+	requires javafx.media;
+	
+	requires java.desktop;
+	
+	requires reactfx;
+	requires controlsfx;
+	requires jdom2;
+	requires org.apache.commons.io;
+	
+	exports core;
+	exports dataStructures;
+	exports screens;
+}
